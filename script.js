1aa5e4605d59ac1678b87f521249b7139b15d9b3
audksamu <61708040+audksamu@users.noreply.github.com>
2020-05-12 20:45:14 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+// question 1
+var firstVar;
+
+// question 2
+var name = "Aud";
+
+// question 3
+var numvar = 46;
+
+// question 4
+var division = 20/5;
+
+// question 5
+typeof "frog";
+
+// question 6
+var orderHasShipped = true;
+
+// question 7
+if(orderHasShipped === true) {
+    console.log("true");
+}
+else {
+    console.log("false");
+}
+
+// question 8
+for (var i = 0; i <= 9; i += 1) {
+    console.log(i);
+}
